a2575a01ca1f786a99cee36e3791d01da04fbaa7
GiannyFrankGaleano <giannyfrank11@gmail.com>
2022-07-02 08:16:27 -0500

    ejemplo while

--- src/MasCiclos.java
+++ src/MasCiclos.java
@@ -1,9 +1,23 @@
 public class MasCiclos {
 
     public static void main(String[] args) {
-        /*comentario
-           otro comentario
-         */
+       //ciclo while
+        // pintar numeros del 1 al 10
+        boolean noHaTerminado = true;
+
+        int numero = 1;
+        while (noHaTerminado){
+
+            System.out.println("Numero "+numero);
+
+            numero = numero+1;
+
+            if(numero>10){
+
+                noHaTerminado = false;
+
+            }
+        }
 
     }
 }
