f413ec81925e28d8742bc0b020d1b37763ff162c
GiannyFrankGaleano <giannyfrank11@gmail.com>
2022-07-02 08:58:23 -0500

    cambio alciclo de numeros y letras

--- src/SegundoEjemplo.java
+++ src/SegundoEjemplo.java
@@ -1,16 +1,15 @@
 public class SegundoEjemplo {
     public static void main(String[] args) {
 
-       int numero = 0;
+        int numero = 0;
 
 
         while (numero < 100) {
-            numero = numero+1;
-            if(numero%2==0){
-                System.out.println("numero: "+numero+" - "+"L");
+            numero = numero + 1;
+            if (numero % 2 == 0) {
+                System.out.println("numero: " + numero + " - " + "L");
 
-            }
-            else {
+            } else {
                 System.out.println("numero: " + numero + " - " + "D");
             }
         }
